e824139833dc5c5e59120d8f6c2ff81c7efacce0
Haris Bhora <bhoraniyaharis963@gmail.com>
2026-07-01 18:13:47 +0530

    create new portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Required for GitHub Pages
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
-  },
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
